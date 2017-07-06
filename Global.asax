bbc487f9899e4986005f444faabd1b3c0154f708
Dhanik <dhanik.sahni@yahoo.com>
2017-07-06 23:05:09 +0530

    First commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeMaster.MvcApplication" Language="C#" %>
---
+++ Repositories/EmployeeInfoRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Unity;
+using EmployeeMaster.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeMaster.Repositories
+{
+    public class EmployeeInfoRepository : IRepository<EmployeeInfo,int>
+    {
+        [Dependency]
+        public ApplicationEntities context{get;set;}
+
+        public IEnumerable<EmployeeInfo> Get()
+        {
+            return context.EmployeeInfoes.ToList();
+        }
+
+        public EmployeeInfo Get(int id)
+        {
+            return context.EmployeeInfoes.Find(id);
+        }
+
+        public void Add(EmployeeInfo entity)
+        {
+            context.EmployeeInfoes.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Remove(EmployeeInfo entity)
+        {
+            var obj = context.EmployeeInfoes.Find(entity.EmpNo);
+            context.EmployeeInfoes.Remove(obj);
+            context.SaveChanges();
+        }
+    }
+}
